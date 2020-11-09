87dc1bbd1346134c71b90bac80c6b690d537ac91
Daniel Veselka <daniel.veselka@gmail.com>
2020-11-09 20:21:01 +0100

    use dveselka for Docker images

--- complex/deploy.sh
+++ complex/deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t dveselka/multi-client:latest -t dveselka/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t dveselka/multi-server:latest -t dveselka/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t dveselka/multi-worker:latest -t dveselka/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push dveselka/multi-client:latest
+docker push dveselka/multi-server:latest
+docker push dveselka/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push dveselka/multi-client:$SHA
+docker push dveselka/multi-server:$SHA
+docker push dveselka/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=dveselka/multi-server:$SHA
+kubectl set image deployments/client-deployment client=dveselka/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dveselka/multi-worker:$SHA
